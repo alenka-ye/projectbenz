e6d24611681dd31058eed95fea62484da84aec11
Алёна <aaaaaalena00@mail.ru>
2023-03-13 10:02:20 +0700

    finally

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import ru_local as ru
-print("Введите объем бензина в галлонах:")
+
+print(ru.initial_value)
 volume = float(input())
+
 liters = volume * 3.785412
 print(ru.liters, round(liters, 2))
 barrels = volume / 19.5
@@ -11,6 +13,7 @@ ethanol = volume * (115000 / 75700)
 print(ru.ethanol, round(ethanol, 2))
 cost = volume * 3
 print(ru.cost, round(cost, 2))
+
 #средний объём потребления нефтепродуктов в России 3699000 баррелей в сутки.
 bar1 = 3699000
 gallon1 = bar1 * 19.5
